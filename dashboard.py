17cdae3e739cc6938428850324de0e5b571daab4
juliocemendozza3-netizen <juliocemendozza3@gmail.com>
2026-06-28 16:30:42 -0500

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -303,10 +303,10 @@ fig.update_layout(
 
 )
 
-    st.plotly_chart(
-        fig,
-        use_container_width=True
-    )
+st.plotly_chart(
+    fig,
+    use_container_width=True
+)
 # =====================================================
 # GRÁFICO DE BARRAS
 # =====================================================
